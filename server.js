22ec73b6bea21aea6644f6e3d9f2742c1960ccba
cCyberBear <duycazami92012001@gmail.com>
2022-06-20 13:58:20 +0700

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const catchError = require("./middlewares/error");
 const { rootRouter } = require("./routes");
 const app = express();
 
-const port = 5001;
+const port = 5000;
 
 app.use(express.json());
 
