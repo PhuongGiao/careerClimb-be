d3f9edfab8723c14fedc3c7eb824779b1192361d
cCyberBear <duycazami92012001@gmail.com>
2022-06-22 11:03:20 +0700

    format-update

--- controllers/bookingUser.js
+++ controllers/bookingUser.js
@@ -1,7 +1,6 @@
-const { Op } = require("sequelize");
 const { BookingUser } = require("../models");
-
-const createBookingUser = async (req, res) => {
+const catchAsync = require("../middlewares/async");
+exports.createBookingUser = catchAsync(async (req, res) => {
   const {
     TenantId,
     Email,
@@ -37,129 +36,46 @@ const createBookingUser = async (req, res) => {
     AppleGivenName,
     AppleUserIdentifier,
   } = req.body;
-  console.log(req.body);
-  try {
-    const newStation = await BookingUser.create({
-      TenantId,
-      Email,
-      Username,
-      Phone,
-      HashPassword,
-      Salt,
-      Fullname,
-      CreatedDate,
-      UpdatedDate,
-      Status,
-      UpdatedBy,
-      Image,
-      FacebookId,
-      GoogleEmail,
-      FacebookToken,
-      FacebookFirstname,
-      FacebookLastname,
-      FacebookEmail,
-      FacebookPicture,
-      GoogleName,
-      GooglePicture,
-      UserTypeId,
-      CreationTime,
-      CreatorUserId,
-      LastModificationTime,
-      LastModifierUserId,
-      IsDeleted,
-      DeleterUserId,
-      DeletionTime,
-      AppleEmail,
-      AppleFamilyName,
-      AppleGivenName,
-      AppleUserIdentifier,
-    });
-    console.log(newStation);
-    res.status(201).send(newStation);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
+  const newStation = await BookingUser.create({
+    TenantId,
+    Email,
+    Username,
+    Phone,
+    HashPassword,
+    Salt,
+    Fullname,
+    CreatedDate,
+    UpdatedDate,
+    Status,
+    UpdatedBy,
+    Image,
+    FacebookId,
+    GoogleEmail,
+    FacebookToken,
+    FacebookFirstname,
+    FacebookLastname,
+    FacebookEmail,
+    FacebookPicture,
+    GoogleName,
+    GooglePicture,
+    UserTypeId,
+    CreationTime,
+    CreatorUserId,
+    LastModificationTime,
+    LastModifierUserId,
+    IsDeleted,
+    DeleterUserId,
+    DeletionTime,
+    AppleEmail,
+    AppleFamilyName,
+    AppleGivenName,
+    AppleUserIdentifier,
+  });
+  console.log(newStation);
+  res.status(201).send(newStation);
+});
 
-const getAllBookingUser = async (req, res) => {
-  //   const { name } = req.query;
-
-  try {
-    const listStation = await BookingUser.findAll();
-    //   console.log(listStation)
-    res.status(200).send(listStation);
-    // if (name) {
-    //   const listStation = await Station.findAll({
-    //     where: {
-    //       name: {
-    //         [Op.like]: `%${name}%`,
-    //       },
-    //     },
-    //   });
-    //   res.status(200).send(listStation);
-    // } else {
-    //   const listStation = await Station.findAll();
-    //   console.log(listStation)
-    //   res.status(200).send(listStation);
-    // }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
-
-const getDetailStation = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const detail = await Station.findAll({
-      where: {
-        id,
-      },
-    });
-    res.status(200).send(detail);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-
-const updateStation = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, address, province } = req.body;
-    const stationUpdate = await Station.findOne({
-      where: {
-        id,
-      },
-    });
-    stationUpdate.name = name;
-    stationUpdate.address = address;
-    stationUpdate.province = province;
-    await stationUpdate.save();
-    res.status(200).send(stationUpdate);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-
-const deleteStation = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Station.destroy({
-      where: {
-        id,
-      },
-    });
-    res.status(200).send("Delete Success!");
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-
-module.exports = {
-  createBookingUser,
-  getAllBookingUser,
-  getDetailStation,
-  updateStation,
-  deleteStation,
-};
+exports.getAllBookingUser = catchAsync(async (req, res) => {
+  const listStation = await BookingUser.findAll();
+  res.status(200).send(listStation);
+});
--- controllers/studioPost.js
+++ controllers/studioPost.js
@@ -1,84 +1,42 @@
-const { Op } = require("sequelize");
 const { StudioPost } = require("../models");
+const catchAsync = require("../middlewares/async");
 
-const getAllStudioPost = async (req, res) => {
-  //   const { name } = req.query;
+exports.getAllStudioPost = catchAsync(async (req, res) => {
+  const listStation = await StudioPost.findAll();
+  res.status(200).send(listStation);
+});
 
-  try {
-    const listStation = await StudioPost.findAll();
-    //   console.log(listStation)
-    res.status(200).send(listStation);
-    // if (name) {
-    //   const listStation = await Station.findAll({
-    //     where: {
-    //       name: {
-    //         [Op.like]: `%${name}%`,
-    //       },
-    //     },
-    //   });
-    //   res.status(200).send(listStation);
-    // } else {
-    //   const listStation = await Station.findAll();
-    //   console.log(listStation)
-    //   res.status(200).send(listStation);
-    // }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
-
-const getDetailStation = async (req, res) => {
+exports.getDetailStation = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    const detail = await Station.findAll({
-      where: {
-        id,
-      },
-    });
-    res.status(200).send(detail);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+  const detail = await Station.findAll({
+    where: {
+      id,
+    },
+  });
+  res.status(200).send(detail);
+});
 
-const updateStation = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, address, province } = req.body;
-    const stationUpdate = await Station.findOne({
-      where: {
-        id,
-      },
-    });
-    stationUpdate.name = name;
-    stationUpdate.address = address;
-    stationUpdate.province = province;
-    await stationUpdate.save();
-    res.status(200).send(stationUpdate);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
-
-const deleteStation = async (req, res) => {
+exports.updateStation = catchAsync(async (req, res) => {
   const { id } = req.params;
-  try {
-    await Station.destroy({
-      where: {
-        id,
-      },
-    });
-    res.status(200).send("Delete Success!");
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
+  const { name, address, province } = req.body;
+  const stationUpdate = await Station.findOne({
+    where: {
+      id,
+    },
+  });
+  stationUpdate.name = name;
+  stationUpdate.address = address;
+  stationUpdate.province = province;
+  await stationUpdate.save();
+  res.status(200).send(stationUpdate);
+});
 
-module.exports = {
-  createStudioPost,
-  getAllStudioPost,
-  getDetailStation,
-  updateStation,
-  deleteStation,
-};
+exports.deleteStation = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  await Station.destroy({
+    where: {
+      id,
+    },
+  });
+  res.status(200).send("Delete Success!");
+});
