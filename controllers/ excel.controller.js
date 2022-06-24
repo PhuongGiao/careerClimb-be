b851c6b61505a6f5ce5b81cca2e99deff6fc2b44
Nguyen Ha Hiep <hiephanguyen4401@gmail.com>
2022-06-24 16:32:06 +0700

    dsadsa

--- controllers/ excel.controller.js
+++ controllers/ excel.controller.js
@@ -1,61 +1,53 @@
 const { District, StudioPost } = require("../models");
 const excel = require("exceljs");
-const downloadExcel = () => {
-  (data = [
-    {
-      id: 1,
-      Name: "hiep",
-      Prefix: 2,
-      ProvinceId: 2,
-      TenantId: 3,
-    },
-    {
-      id: 1,
-      Name: "hiep",
-      Prefix: 2,
-      ProvinceId: 2,
-      TenantId: 3,
-    },
-  ]),
-    "hiuep";
-  return (req, res) => {
-    console.log("dsa-d-sa-dsa-d-sa-ds-a-dsa");
-    //   StudioPost.findAll().then((objs) => {
-    //     let data = [];
-    //     objs.forEach((obj) => {
-    //       //   data.push({
-    //       //     id: obj.id,
-    //       //     Name: obj.Name,
-    //       //     Prefix: obj.Prefix,
-    //       //     ProvinceId: obj.ProvinceId,
-    //       //     TenantId: obj.TenantId,
-    //       //   });
-    //       data.push(obj);
-    //     });
+const downloadExcel = (req, res) => {
+  //   let data = [
+  //     n{
+  //       id: 1,
+  //       Name: "hiep",
+  //       Prefix: 2,
+  //       ProvinceId: 2,
+  //       TenantId: 3,
+  //     },
+  //     {
+  //       id: 4,
+  //       Name: "hiep",
+  //       Prefix: 2,
+  //       ProvinceId: 2,
+  //       TenantId: 3,
+  //     },
+  //     {
+  //       id: 3,
+  //       Name: "hiep",
+  //       Prefix: 2,
+  //       ProvinceId: 2,
+  //       TenantId: 3,
+  //     },
+  //     {
+  //       id: 2,
+  //       Name: "hiep",
+  //       Prefix: 2,
+  //       ProvinceId: 2,
+  //       TenantId: 3,
+  //     },
+  //   ];
 
-    //     let workbook = new excel.Workbook();
-    //     let worksheet = workbook.addWorksheet("Districts");
-    //     console.log(Object.keys(data[0].dataValues));
-    //     worksheet.columns = Object.keys(data[0].dataValues).map((obj) => {
-    //       return { header: obj, key: obj, width: 15 };
-    //     });
+  StudioPost.findAll().then((objs) => {
+    let data = [];
+    objs.forEach((obj) => {
+      //   data.push({
+      //     id: obj.id,
+      //     Name: obj.Name,
+      //     Prefix: obj.Prefix,
+      //     ProvinceId: obj.ProvinceId,
+      //     TenantId: obj.TenantId,
+      //   });
+      data.push(obj);
+    });
+    console.log(data);
 
-    //     // Add Array Rows
-    //     worksheet.addRows(data);
-    //     res.setHeader(
-    //       "Content-Type",
-    //       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    //     );
-    //     res.setHeader(
-    //       "Content-Disposition",
-    //       "attachment; filename=" + "data.xlsx"
-    //     );
-    //     return workbook.xlsx.write(res).then(function () {
-    //       res.status(200).end();
-    //     });
-    //   });
     let workbook = new excel.Workbook();
-    let worksheet = workbook.addWorksheet(name);
+    let worksheet = workbook.addWorksheet("Districts");
     console.log(Object.keys(data[0].dataValues));
     worksheet.columns = Object.keys(data[0].dataValues).map((obj) => {
       return { header: obj, key: obj, width: 15 };
@@ -67,14 +59,31 @@ const downloadExcel = () => {
       "Content-Type",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     );
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=" + `${name}.xlsx`
-    );
+    res.setHeader("Content-Disposition", "attachment; filename=" + "data.xlsx");
     return workbook.xlsx.write(res).then(function () {
       res.status(200).end();
     });
-  };
+  });
+
+  //   console.log(Object.keys(data[0]));
+  //   console.log(data);
+  //   let workbook = new excel.Workbook();
+  //   let worksheet = workbook.addWorksheet("Districts");
+  //   console.log(Object.keys(data[0].dataValues));
+  //   worksheet.columns = Object.keys(data[0]).map((obj) => {
+  //     return { header: obj, key: obj, width: 15 };
+  //   });
+
+  //   // Add Array Rows
+  //   worksheet.addRows(data);
+  //   res.setHeader(
+  //     "Content-Type",
+  //     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  //   );
+  //   res.setHeader("Content-Disposition", "attachment; filename=" + "data.xlsx");
+  //   return workbook.xlsx.write(res).then(function () {
+  //     res.status(200).end();
+  //   });
 };
 module.exports = {
   downloadExcel,
