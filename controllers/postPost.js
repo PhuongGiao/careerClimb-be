6d5cff5ac0eaa79427f98b8a219c54a1175b7723
cCyberBear <duycazami92012001@gmail.com>
2022-06-30 11:28:26 +0700

    filterPost

--- controllers/postPost.js
+++ controllers/postPost.js
@@ -2,11 +2,12 @@ const { AppBinaryObject, sequelize } = require("../models");
 const { Post } = require("../models");
 
 const catchAsync = require("../middlewares/async");
-const Pagination = require("../utils/pagination");
+const { Op } = require("sequelize");
 const ApiError = require("../utils/ApiError");
 
 exports.postPost = catchAsync(async (req, res) => {
-  const { Tags, Description } = req.body;
+  let { Tags, Description } = req.body;
+  Tags = Tags.split(",").sort().join(",");
   let listImage = [];
   await Promise.all(
     req.files.map(async (val) => {
@@ -32,8 +33,33 @@ exports.postPost = catchAsync(async (req, res) => {
 });
 
 exports.getAllPost = catchAsync(async (req, res) => {
-  const { page, limit } = req.query;
-  let total = await Post.count({});
+  let { page, limit, tags } = req.query;
+  const constant = [
+    "studio",
+    "makeup",
+    "nguoimau",
+    "nhiepanh",
+    "thietbi",
+    "trangphuc",
+  ];
+  let where;
+  let rightOption;
+  if (tags) {
+    const rawOptions = tags.split(",");
+    rightOption = rawOptions
+      .filter((option) => constant.indexOf(option) !== -1)
+      .filter((option, idx) => rawOptions.indexOf(option) === idx)
+      .sort()
+      .join(",");
+    where = {
+      Tags: {
+        [Op.like]: `%${rightOption}%`,
+      },
+    };
+  }
+  console.log(where);
+  let total = await Post.count({ where });
+  console.log(total);
   if (+limit <= 0 || isNaN(+limit) || +limit >= 20) {
     limit = 1;
   }
@@ -46,9 +72,10 @@ exports.getAllPost = catchAsync(async (req, res) => {
     page = 1;
   }
   const newList = await sequelize.query(
-    "SELECT Posts.Id as Id,Posts.Tags,Posts.Description,Posts.Image1,Posts.Image2,Posts.Image3,Posts.Image4,Posts.Image5,Posts.Image6,Posts.TotalLikes,Posts.TotalComments,Posts.CreationTime,BookingUsers.Username,BookingUsers.Image as Avatar FROM Posts INNER JOIN BookingUsers ON  BookingUsers.Id = Posts.BookingUserId ORDER BY Posts.Id DESC LIMIT :limit OFFSET :offset",
+    "SELECT Posts.Id as Id,Posts.Tags,Posts.Description,Posts.Image1,Posts.Image2,Posts.Image3,Posts.Image4,Posts.Image5,Posts.Image6,Posts.TotalLikes,Posts.TotalComments,Posts.CreationTime,BookingUsers.Username,BookingUsers.Image as Avatar FROM Posts INNER JOIN BookingUsers ON  BookingUsers.Id = Posts.BookingUserId WHERE Posts.Tags LIKE :string ORDER BY Posts.Id DESC LIMIT :limit OFFSET :offset",
     {
       replacements: {
+        string: "%" + rightOption + "%",
         offset: +skip,
         limit: +limit,
       },
